d7d686112da37d3cb5695711a0c17043bb3b10e4
hyosang <hyomee@naver.com>
2023-03-16 01:51:16 +0900

    기본 리펙토링

--- src/main/java/com/hyomee/example35/e01/event/EventSoCGroup35.java
+++ src/main/java/com/hyomee/example35/e01/event/EventSoCGroup35.java
@@ -24,18 +24,24 @@ public class EventSoCGroup35 {
     public BenefitVO wirelessSubscriberEvent(SaleEventVO saleEventVO) {
 
         ValidationPlanService.PolicyVO policyVO = validationPlanService.validationPolicy(saleEventVO);
-        if (StringUtils.isNotEmpty(policyVO.getBenefitVO().getReason()))
-            return policyVO.getBenefitVO();
+        if (StringUtils.isNotEmpty(policyVO.getBenefitVO().getReason())) {
+            BenefitVO benefitVO = policyVO.getBenefitVO().toBuilder().build();
+            policyVO = null; // 내부 비정적 클래스는 메모리 누수 발행할 수 있으므로 null 초가화 한다,
+            return benefitVO;
+        }
 
+        String addProductCode =  policyVO.getEventBenefit().getAddProductCode();
+        int deviceDcPrice = policyVO.getEventBenefit().getDeviceDcPrice();
         // 3. 혜택 조건 체크
         BenefitVO benefitVO = validationPlanService.validationPlan(saleEventVO, policyVO.getEventModelSenio());
-
+        policyVO = null; // 내부 비정적 클래스는 메모리 누수 발행할 수 있으므로 null 초가화 한다,
+        //System.gc();
         // 4 정책 반영
         return StringUtils.isEmpty(benefitVO.getReason()) ?
                 BenefitVO.builder()
                          .reason("")
-                         .productCode(policyVO.getEventBenefit().getAddProductCode())
-                         .devicePrice(saleEventVO.getDevicePrice() - policyVO.getEventBenefit().getDeviceDcPrice())
+                         .productCode(addProductCode)
+                         .devicePrice(saleEventVO.getDevicePrice() - deviceDcPrice)
                          .build() :
                 benefitVO ;
     }
